845910d5a53627dcc28465809712685c992ed549
Matthew Harrison <matthewharrison@Matthews-MacBook-Pro-2.local>
2021-01-13 18:28:32 -0500

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>classs</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
